1991dafc3f1a3ff78f4217a2c6afb313ed7178bf
Ashlyn Carter <eannacarter@gmail.com>
2024-09-30 10:29:05 -0400

    Same as last message

--- index.js
+++ index.js
@@ -2,21 +2,25 @@
 
 //Create the gameboard
 //Set both scores to 0
-//Display the scores
-//Set max speed for paddles
-
 //Create the ball
-//Take constant input from keyboard for player, move player paddle accordingly
-//Once player moves for the first time, start the ball moving towards computer
+//Take constant input from keyboard for player
+    // move player paddle accordingly
+
+//GAME LOOP
+    //Set max speed for paddles
+    //Display the scores
+    //Reset ball to center
+    //Once player moves for the first time 
+        //start the ball moving towards computer
 
 
-//REPEAT UNTIL COMPUTER OR PLAYER MISSES
+    //REPEAT UNTIL COMPUTER OR PLAYER MISSES
 
-//WHEN BALL IS ON COMPUTER SIDE:
-//Move computer paddle to same height as ball
-//If computer paddle hits ball, reverse it.
-//If computer paddle misses it, restart and add 1 to computer score.
-
-//If player paddle hits ball, reverse it. 
-//If player paddle misses it, restart and add 1 to player score.
+        //WHEN BALL IS ON COMPUTER SIDE:
+            //Move computer paddle to same height as ball
+            //If computer paddle hits ball, reverse it.
+            //If computer paddle misses it, restart and add 1 to computer score.
+        //WHEN BALL IS ON PLAYER SIDE:
+            //If player paddle hits ball, reverse it. 
+            //If player paddle misses it, restart and add 1 to player score.
 
