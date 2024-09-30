d4ed43c3d5957f1045e4d53a4323c66529275485
Ashlyn Carter <eannacarter@gmail.com>
2024-09-30 10:28:15 -0400

    Fixed issues and added indents to make loops and if statements more clear

--- index.js
+++ index.js
@@ -3,17 +3,20 @@
 //Create the gameboard
 //Set both scores to 0
 //Display the scores
+//Set max speed for paddles
 
 //Create the ball
-//Take input from the arrow keys for player
+//Take constant input from keyboard for player, move player paddle accordingly
 //Once player moves for the first time, start the ball moving towards computer
 
 
 //REPEAT UNTIL COMPUTER OR PLAYER MISSES
-//Move computer paddle to ball
+
+//WHEN BALL IS ON COMPUTER SIDE:
+//Move computer paddle to same height as ball
 //If computer paddle hits ball, reverse it.
 //If computer paddle misses it, restart and add 1 to computer score.
 
 //If player paddle hits ball, reverse it. 
-//If player paddle hits misses it, restart and add 1 to player score.
+//If player paddle misses it, restart and add 1 to player score.
 
