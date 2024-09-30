7cae6b3dc19fe9bddf8b582bb53b781f6a23868e
Ashlyn Carter <eannacarter@gmail.com>
2024-09-30 10:17:51 -0400

    Added Seudo-code for Pong

---
+++ index.js
@@ -0,0 +1,19 @@
+//PONG
+
+//Create the gameboard
+//Set both scores to 0
+//Display the scores
+
+//Create the ball
+//Take input from the arrow keys for player
+//Once player moves for the first time, start the ball moving towards computer
+
+
+//REPEAT UNTIL COMPUTER OR PLAYER MISSES
+//Move computer paddle to ball
+//If computer paddle hits ball, reverse it.
+//If computer paddle misses it, restart and add 1 to computer score.
+
+//If player paddle hits ball, reverse it. 
+//If player paddle hits misses it, restart and add 1 to player score.
+
